25788449867633c17c1403d43e1f9508b1c7316d
Randy Wang <rwang@amobee.com>
2016-06-16 00:36:46 -0700

    caching RDDs and improve pom
    adding different pom files for vm and gx env
    
    adding more println for steps

--- src/main/scala/io/bittiger/movierating/hollywood/MovieRater.scala
+++ src/main/scala/io/bittiger/movierating/hollywood/MovieRater.scala
@@ -1,15 +1,8 @@
 package io.bittiger.movierating.hollywood
 
-import java.io.File
-import java.net.URL
-
 import org.apache.spark.rdd._
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
-//import org.apache.spark.sql.SQLContext
-
-
-import scala.sys.process._
 
 
 /**
@@ -31,25 +24,25 @@ object MovieRater extends App {
   val conf = new SparkConf().setAppName("MovieRater")
 //    .set("spark.executor.memory", "2g")
   val sc = new SparkContext(conf)
-//  val sqlContext = new SQLContext(sc)
 
   val ratingFilePath = args(0)
 //  val movieFilePath = args(1)
+  println("========================== STARTING ==============================" )
 
   // load ratings and movie titles, and parse
   val ratingsData = sc.textFile(ratingFilePath)
-  //remove header
+  //remove csv header
   val noHeader = ratingsData.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
   val ratingsRDD = noHeader.map(_.split(',') match { case Array(user, movie, rating, timestamp) =>
     Rating(user.toInt, movie.toInt, rating.toDouble)
   })
+  ratingsRDD.cache
 
-  val numRatings = ratingsRDD.count()
-  val numUsers = ratingsRDD.map(_.user).distinct().count()
-  val numMovies = ratingsRDD.map(_.product).distinct().count()
-
-  println("Got " + numRatings + " ratings from " + numUsers
-    + " users on " + numMovies + " movies.")
+  val numRatings = ratingsRDD.count
+  val numUsers = ratingsRDD.map(_.user).distinct.count
+  val numMovies = ratingsRDD.map(_.product).distinct().count
+  println("Got " + numRatings + " ratings from " + numUsers + " users on "
+    + numMovies + " movies.")
 
   //split into 3 for training, validation, and testing
   val splitArr = ratingsRDD.randomSplit(Array(6, 2, 2), 0L)
@@ -57,14 +50,18 @@ object MovieRater extends App {
   val validationRDD = splitArr(1)
   val testRDD = splitArr(2)
 
-  val numTraining = trainingRDD.count()
-  val numValidation = validationRDD.count()
-  val numTest = testRDD.count()
+  //persist frequently used RDDs
+  trainingRDD.cache
+  validationRDD.cache
+  testRDD.cache
 
+  val numTraining = trainingRDD.count
+  val numValidation = validationRDD.count
+  val numTest = testRDD.count
   println("Training: " + numTraining + ", validation: " + numValidation + ", test: " + numTest)
 
   // train models and evaluate them on the validation set
-
+  println("=============>>>> Training... <<<<=================")
   val ranks = List(8, 12)
   val lambdas = List(0.1, 10.0)
   val numIters = List(10, 20)
@@ -88,18 +85,20 @@ object MovieRater extends App {
   }
 
   // evaluate the best model on the test set
+  println("=============>>>> Evaluating... <<<<=================")
   val testRmse = computeRmse(bestModel.get, testRDD, numTest)
   println("The best model was trained with rank = " + bestRank + " and lambda = " + bestLambda
     + ", and numIter = " + bestNumIter + ", and its RMSE on the test set is " + testRmse + ".")
 
   // create a naive baseline and compare it with the best model
-
+  println("=============>>>> Validating... <<<<=================")
   val meanRating = trainingRDD.union(validationRDD).map(_.rating).mean
   val baselineRmse =
     math.sqrt(testRDD.map(x => (meanRating - x.rating) * (meanRating - x.rating)).mean)
   val improvement = (baselineRmse - testRmse) / baselineRmse * 100
   println("The best model improves the baseline by " + "%1.2f".format(improvement) + "%.")
 
+  println("========================== COMPLETED ==============================" )
 
   /** Compute RMSE (Root Mean Squared Error). */
   def computeRmse(model: MatrixFactorizationModel, data: RDD[Rating], n: Long): Double = {
@@ -109,6 +108,5 @@ object MovieRater extends App {
       .values
     math.sqrt(predictionsAndRatings.map(x => (x._1 - x._2) * (x._1 - x._2)).reduce(_ + _) / n)
   }
-
 
 }
